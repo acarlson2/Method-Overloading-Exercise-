78a9da7b19825e180391f1eb8755f086922b1294
Aaron Carlson <acarlson2@carthage.edu>
2024-06-07 13:41:52 -0500

    exercise complete

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(1,2));
+            Console.WriteLine(Add(1.5m,2.3m));
+            Console.WriteLine(Add(1,2,false));
+            Console.WriteLine(Add(0,1,true));
+            Console.WriteLine(Add(1,2,true));
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool isMoney)
+        {
+            string retVal;
+            int sum = num1 + num2;
+
+            if(isMoney)
+            {
+                if(sum == 1)
+                {
+                    retVal = $"{sum} dollar";
+                }
+                else
+                {
+                    retVal = $"{sum} dollars";
+                }
+            }
+            else
+            {
+                retVal = $"{sum}";
+            }
+
+            return retVal;
         }
     }
 }
